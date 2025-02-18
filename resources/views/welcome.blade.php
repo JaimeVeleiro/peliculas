4dd28fc449af7280752ea61d532106f3ef85776d
JaimeVeleiro <145908255+JaimeVeleiro@users.noreply.github.com>
2025-02-18 10:28:25 +0100

    test9

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel_subido8</title>
+        <title>Laravel_subido9</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
